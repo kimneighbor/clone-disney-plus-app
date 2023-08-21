b8ce4079f8feb9fcbc524cf99b482d502b2c701d
kimneighbor <designkimdh@gmail.com>
2023-08-21 23:47:10 +0900

    add NavWrapper

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 
+interface NavWrapperProps {
+    show: boolean;
+}
+
 const Nav = () => {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        })
+        return () => {
+            window.removeEventListener("scroll", () => {
+            });
+        };
+    }, []);
+
+
     return (
-        <NavWrapper>
+        <NavWrapper show={show}>
             <Logo>
                 <img
                     alt="Disney Plus Logo"
@@ -17,13 +38,13 @@ const Nav = () => {
 
 export default Nav;
 
-const NavWrapper = styled.nav`
+const NavWrapper = styled.nav<NavWrapperProps>`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props => props.show ? "#090b13":"transparent"};
   display: flex;
   justify-content: space-between;
   align-items: center;
