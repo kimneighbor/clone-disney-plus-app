ddc62e802c88f8398216ece89183fe19bdd3b633
kimneighbor <designkimdh@gmail.com>
2023-08-22 08:20:50 +0900

    update Banner.tsx

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -44,17 +44,20 @@ const Banner = () => {
 
     if (isClicked) {
         return (
-            <Container>
-                <HomeContainer>
-                    <Iframe
-                        src={`https://www.youtube.com/embed/${movie?.videos?.results[0]?.key}?controls=0&autoplay=1&mute=1&loop=1&mute=1&playlist=${movie?.videos?.results[0]?.key}`}
-                        width="640"
-                        height="360"
-                        frameBorder="0"
-                        allow="autoplay; fullscreen"
-                    ></Iframe>
-                </HomeContainer>
-            </Container>
+            <>
+                <Container>
+                    <HomeContainer>
+                        <Iframe
+                            src={`https://www.youtube.com/embed/${movie?.videos?.results[0]?.key}?controls=0&autoplay=1&mute=1&loop=1&mute=1&playlist=${movie?.videos?.results[0]?.key}`}
+                            width="640"
+                            height="360"
+                            frameBorder="0"
+                            allow="autoplay; fullscreen"
+                        ></Iframe>
+                    </HomeContainer>
+                </Container>
+                <button onClick={() => setIsClicked(false)}>X</button>
+            </>
         );
     } else {
 
