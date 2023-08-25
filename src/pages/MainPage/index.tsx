e1934f0a8ff17ca1d797846c248222e83e4a89ae
kimneighbor <designkimdh@gmail.com>
2023-08-25 16:29:03 +0900

    update MainPage

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Nav from "../../component/Nav";
+import Banner from "../../component/Banner";
+import Category from "../../component/Category";
+import Row from "../../component/Row";
+import requests from "../../api/request";
+import styled from "styled-components";
+
+const MainPage = () => {
+    return (
+        <Container>
+            <Nav/>
+            <Banner/>
+            <Category/>
+            <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+
+        </Container>
+    );
+};
+
+export default MainPage;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+
+  }
+
+`
